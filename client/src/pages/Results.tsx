d7547ee101c2847379ab763df86a87c0ed5e7ce2
xcjackpan <xcjackpan99@gmail.com>
2019-06-10 23:56:53 -0400

    solution

--- client/src/pages/Results.tsx
+++ client/src/pages/Results.tsx
@@ -41,11 +41,22 @@ class Results extends Component<any, any> {
     }: { term: number; courseCode: string } = this.props.match.params;
     const { subject, courseNumber } = this.getCourse(courseCode);
 
-    axios
-      .get(`http://localhost:3001/search/${term}/${subject}/${courseNumber}`)
-      .then(res => {
-        this.setState({ results: res.data });
-      });
+    if (
+      this.state.course &&
+      this.state.course !== `${subject.toUpperCase()}${courseNumber}`
+    ) {
+      axios
+        .get(`http://localhost:3001/search/${term}/${subject}/${courseNumber}`)
+        .then(res => {
+          this.setState(
+            {
+              results: res.data,
+              course: `${subject.toUpperCase()}${courseNumber}`
+            },
+            () => this.forceUpdate()
+          );
+        });
+    }
   };
 
   getCourse = (
@@ -59,7 +70,43 @@ class Results extends Component<any, any> {
     };
   };
 
+  parse_results = (results: [any]): any[] => {
+    let newResults: any[] = [];
+    if (!results) {
+      return newResults;
+    }
+    for (let i = 0; i < results.length; i++) {
+      if (!results[i].reserve && results[i + 1] && results[i + 1].reserve) {
+        var reserve_enrol_total = 0;
+        var reserve_enrol_cap = 0;
+        let temp_index = i + 1;
+        while (results[temp_index] && results[temp_index].reserve) {
+          reserve_enrol_cap += results[temp_index].reserve_enrol_cap;
+          reserve_enrol_total += results[temp_index].reserve_enrol_total;
+          temp_index++;
+        }
+        newResults.push(results[i]);
+        if (results[i].enrol_cap - reserve_enrol_cap > 0) {
+          newResults.push({
+            section: `${results[i].section} RES0`,
+            reserve: "Not reserved",
+            reserve_enrol_cap: results[i].enrol_cap - reserve_enrol_cap,
+            reserve_enrol_total: results[i].enrol_total - reserve_enrol_total,
+            instructor: results[i].instructor,
+            time: results[i].time,
+            days: results[i].days,
+            date: results[i].date
+          });
+        }
+      } else {
+        newResults.push(results[i]);
+      }
+    }
+    return newResults;
+  };
+
   render() {
+    this.parse_results(this.state.results);
     return (
       <div className="results-page">
         <div className="top-bar">
@@ -74,7 +121,7 @@ class Results extends Component<any, any> {
           />
           <HelpCircle id="help-icon" onClick={this.props.help} />
         </div>
-        <ResultsTable results={this.state.results} />
+        <ResultsTable results={this.parse_results(this.state.results)} />
       </div>
     );
   }
